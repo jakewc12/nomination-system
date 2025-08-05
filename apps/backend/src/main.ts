87d23650b1c4c17c31bcacb0f1f402382884da0c
Jake Wu-Chen <jwcgolf12@gmail.com>
2025-08-04 21:08:22 -0400

    updated package

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -10,24 +10,28 @@ import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
 
-  const globalPrefix = "api";
-  app.setGlobalPrefix(globalPrefix);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-    })
-  );
+    const globalPrefix = "api";
+    app.setGlobalPrefix(globalPrefix);
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+      })
+    );
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port, "0.0.0.0");
+    const port = process.env.PORT || 3000;
+    await app.listen(port, "0.0.0.0");
 
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+    Logger.log(
+      `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    );
+  } catch (error) {
+    console.error("❌ Error during app bootstrap:", error);
+  }
 }
 
 bootstrap();
